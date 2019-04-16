5661e6bfd9b8c671b39d968429c075a52aede72a
SyAtlantis <1102960695@qq.com>
2019-04-16 18:09:24 +0800

    修改二级密码

--- src/base/tr_second.js
+++ src/base/tr_second.js
@@ -3,7 +3,7 @@ const ByteBuffer = require("bytebuffer");
 class Second {
 
     static calculateFee() {
-        return 10000000;
+        return 500000000;
     }
 
     static create(data, tr) {
@@ -18,7 +18,7 @@ class Second {
     static getBytes(tr) {
         try {
             var bb = new ByteBuffer(32, true);
-            var publicKeyBuffer = new Buffer(trs.asset.signature.publicKey, 'hex');
+            var publicKeyBuffer = new Buffer(tr.asset.signature.publicKey, 'hex');
 
             for (var i = 0; i < publicKeyBuffer.length; i++) {
                 bb.writeByte(publicKeyBuffer[i]);
--- test/test-trs.js
+++ test/test-trs.js
@@ -1,10 +1,22 @@
 const trs = require("../src/base/transaction")
+const crypto = require("crypto")
+const ed = require("../src/utils/ed-nacl")
 
+// let tr = trs.createTransaction({
+//     type: 0,
+//     secret: "race forget pause shoe trick first abuse insane hope budget river enough",
+//     amount: 1222222,
+//     recipientId: "A79wqbYgZC5Bb923wWDXKjD7KBDn5BD6gg"
+// })
+
+
+var secondHash = crypto.createHash('sha256').update("asd", 'utf8').digest();
+var secondKeypair = ed.MakeKeypair(secondHash);
 let tr = trs.createTransaction({
-    type: 0,
-    secret: "race forget pause shoe trick first abuse insane hope budget river enough",
-    amount: 1222222,
-    recipientId: "A79wqbYgZC5Bb923wWDXKjD7KBDn5BD6gg"
+    type: 1,
+    secret: "immense buffalo organ pond illegal erupt prepare arrow cliff fit abstract task",
+    secondSecret: "asd",
+    secondKeypair
 })
 
 console.log(JSON.stringify(tr))
