d472943e8766390a0dd087115bef559e9533f608
SyAtlantis <1102960695@qq.com>
2019-04-17 10:09:16 +0800

    ed

--- index.js
+++ index.js
@@ -2,8 +2,7 @@ module.exports = {
     transaction: require("./src/base/transaction"),
     crypto: require("crypto"),
     utils: {
-        ed: require("./src/utils/ed"),
-        ed_nacl: require("./src/utils/ed-nacl")
+        ed: require("./src/utils/ed-nacl")
     }
 
 }
