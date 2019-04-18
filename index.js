96f0579c1911224ae5967eae94b82632fecf1b59
SyAtlantis <1102960695@qq.com>
2019-04-18 09:45:52 +0800

    添加address,base58check,bignumber

--- index.js
+++ index.js
@@ -2,7 +2,9 @@ module.exports = {
     transaction: require("./src/base/transaction"),
     crypto: require("crypto"),
     utils: {
-        ed: require("./src/utils/ed-nacl")
+        ed: require("./src/utils/ed-nacl"),
+        slots: require("./src/utils/slots"),
+        address: require("./src/utils/address"),
+        bignumber: require("./src/utils/bignumber")
     }
-
 }
